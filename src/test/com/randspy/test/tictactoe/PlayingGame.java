a889d849c190af7699a6e7481b6ed7c74d7fb270
sgluter <mos.koziel@gmail.com>
2015-08-17 15:41:13 +0200

    First dummy test written.

---
+++ src/test/com/randspy/test/tictactoe/PlayingGame.java
@@ -0,0 +1,13 @@
+package com.randspy.test.tictactoe;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlayingGame {
+
+    @Test
+    public void nothing() {
+        assertEquals(1, 1);
+    }
+}
