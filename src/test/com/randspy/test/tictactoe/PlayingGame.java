616edf9e6cf7352472964ccd4aee704ef49b7bc0
sgluter <mos.koziel@gmail.com>
2015-08-17 17:01:10 +0200

    Mockito added.

--- src/test/com/randspy/test/tictactoe/PlayingGame.java
+++ src/test/com/randspy/test/tictactoe/PlayingGame.java
@@ -1,6 +1,12 @@
 package com.randspy.test.tictactoe;
 
+import static org.mockito.Mockito.*;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -8,6 +14,14 @@ public class PlayingGame {
 
     @Test
     public void nothing() {
-        assertEquals(1, 1);
+
+        // mock creation
+        List mockedList = mock(List.class);
+
+        mockedList.add("one");
+        mockedList.clear();
+
+        verify(mockedList).add("one");
+        verify(mockedList).clear();
     }
 }
