0be9d2de413368e117468ac117533886cec512d7
sgluter <mos.koziel@gmail.com>
2015-08-18 12:09:59 +0200

    Code duplications in PlayingGameTest removed.

--- src/test/com/randspy/test/tictactoe/PlayingGameTest.java
+++ src/test/com/randspy/test/tictactoe/PlayingGameTest.java
@@ -3,8 +3,10 @@ package com.randspy.test.tictactoe;
 import com.randspy.tictactoe.Board;
 import com.randspy.tictactoe.Game;
 import com.randspy.tictactoe.Player;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.Optional;
@@ -14,32 +16,34 @@ import static org.mockito.Mockito.*;
 @RunWith(MockitoJUnitRunner.class)
 public class PlayingGameTest {
 
-    @Test
-    public void playerMakesValidMove() {
+    private Game game;
+    private Optional<Board> board;
 
-        Player player = mock(Player.class);
-
-        Optional<Board> board = Optional.of(new Board());
-        when(player.makesMove()).thenReturn(board, withNoBoardGameIsFinished());
-
-        Game game = new Game(player);
-        game.play();
-        verify(player, times(2)).makesMove();
-    }
+    @Mock
+    private Player player;
 
     private Optional<Board> withNoBoardGameIsFinished() {
         return Optional.<Board>empty();
     }
 
+    @Before
+    public void setUp() throws Exception {
+        board = Optional.of(new Board());
+        game = new Game(player);
+    }
+
+    @Test
+    public void playerMakesValidMove() {
+        when(player.makesMove()).thenReturn(board, withNoBoardGameIsFinished());
+
+        game.play();
+        verify(player, times(2)).makesMove();
+    }
+
     @Test
     public void playerMakesManyValidMoves() {
-
-        Player player = mock(Player.class);
-
-        Optional<Board> board = Optional.of(new Board());
         when(player.makesMove()).thenReturn(board, board, withNoBoardGameIsFinished());
 
-        Game game = new Game(player);
         game.play();
         verify(player, times(3)).makesMove();
     }
