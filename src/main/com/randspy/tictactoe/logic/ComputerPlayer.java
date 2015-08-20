2ed7459e73e511c43952b90caa6ccccfc5335821
sgluter <mos.koziel@gmail.com>
2015-08-20 14:24:52 +0200

    Computer winning game.

--- src/main/com/randspy/tictactoe/logic/ComputerPlayer.java
+++ src/main/com/randspy/tictactoe/logic/ComputerPlayer.java
@@ -5,16 +5,28 @@ import java.util.Optional;
 public class ComputerPlayer extends Player {
     private MinMax ai;
     private Display display;
+    private GameResult gameResult;
 
-    public ComputerPlayer(MinMax ai, Display display) {
+    public ComputerPlayer(MinMax ai, Display display, GameResult gameResult) {
         this.ai = ai;
         this.display = display;
+        this.gameResult = gameResult;
     }
 
     @Override
     public Optional<Board> makesMove(Board board) {
+
         board.setPlayerAtPosition(getId(), ai.move(board));
+        Optional<PlayerId> playerId = gameResult.winnerIs(board);
+
         display.displayBoard(board);
-        return Optional.of(board);
+
+        if (playerId.isPresent()) {
+            display.displayPlayerWon(getId());
+            return Optional.empty();
+        }
+        else {
+            return Optional.of(board);
+        }
     }
 }
--- src/test/com/randspy/test/tictactoe/logic/ComputerIsPlayingTest.java
+++ src/test/com/randspy/test/tictactoe/logic/ComputerIsPlayingTest.java
@@ -10,6 +10,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -21,10 +22,12 @@ public class ComputerIsPlayingTest {
     private MinMax ai;
     @Mock
     private Display display;
+    @Mock
+    private GameResult gameResult;
 
     @Before
     public void setUp() throws Exception {
-        player = new ComputerPlayer(ai, display);
+        player = new ComputerPlayer(ai, display, gameResult);
         board = new Board();
     }
 
@@ -32,10 +35,23 @@ public class ComputerIsPlayingTest {
     public void validMove() {
 
         when(ai.move(board)).thenReturn(new PositionOnBoard(1, 1));
+        when(gameResult.winnerIs(board)).thenReturn(Optional.empty());
+
         Board resultBoard = player.makesMove(board).get();
 
         assertEquals(resultBoard.getPlayerAtPosition(new PositionOnBoard(1, 1)), player.getId());
 
         verify(display).displayBoard(board);
+    }
+
+    @Test
+    public void winningGame() {
+
+        when(ai.move(board)).thenReturn(new PositionOnBoard(1, 1));
+        when(gameResult.winnerIs(board)).thenReturn(Optional.of(player.getId()));
+
+        assertFalse(player.makesMove(board).isPresent());
+        verify(display).displayBoard(board);
+        verify(display).displayPlayerWon(player.getId());
     }
 }
